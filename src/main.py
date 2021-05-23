7bfbadec8e32c0e36987d27581a22fe3694d4a17
fourside <fourside@gmail.com>
2021-05-23 13:49:15 +0900

    cli option parse

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 import sys
 from parse_input import parseInput
+from cli import parseArgs
 
 def main() -> None:
 	try:
+		collectionName = parseArgs(sys.argv[1:])
+		if collectionName is None:
+			raise Exception('not passed collection with `--collection`')
 		jsonData = parseInput(sys.stdin)
-		print(jsonData)
+		print(jsonData, collectionName)
 	except Exception as error:
 		print("Error:", error)
 		sys.exit(-1)
