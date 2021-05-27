8f3cda6c2038a6fe754a7ecc0468f7e1144a4ae1
fourside <fourside@gmail.com>
2021-05-27 23:01:41 +0900

    read json file and add date

--- src/main.go
+++ src/main.go
@@ -1,11 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
+	"regexp"
+	"time"
 )
 
 func main() {
-	fmt.Println('y')
-	os.Exit(0)
+	files, err := filepath.Glob("./json/*.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filePattern := regexp.MustCompile("^(.+?)\\..+")
+	for _, file := range files {
+		fmt.Println(file)
+		match := filePattern.FindStringSubmatch(file)
+		fmt.Println(match[1])
+
+		bytes, err := os.ReadFile(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var result map[string]interface{}
+		json.Unmarshal(bytes, &result)
+		result["date"] = time.Now().Format(time.RFC3339)
+		fmt.Println(result)
+	}
 }
